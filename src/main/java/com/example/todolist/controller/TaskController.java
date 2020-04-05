7546bc871a71b43ad460d3d86560f5b53a406d2e
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-04-05 18:21:44 +0300

    all added RestControllers

--- src/main/java/com/example/todolist/controller/TaskController.java
+++ src/main/java/com/example/todolist/controller/TaskController.java
@@ -2,23 +2,47 @@ package com.example.todolist.controller;
 
 import com.example.todolist.domain.Task;
 import com.example.todolist.repository.TaskRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
 
+    private final TaskRepository taskRepository;
+
     @Autowired
-    TaskRepository taskRepository;
+    public TaskController(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
 
     @PostMapping
     @RequestMapping("/task/")
-    public Long add(Task task){
+    public Task add(@RequestBody Task task){
 
-        Task newTask = taskRepository.save(task);
-        return newTask.getId();
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/task/{id}")
+    public Task getInfo(@PathVariable("id") Task task){
+
+        return task;
+    }
+
+    @PutMapping("/task/{id}")
+    public Task update(
+            @PathVariable("id") Task taskFromDB,
+            @RequestBody Task task
+    ){
+        BeanUtils.copyProperties(task, taskFromDB, "id");
+
+        return taskRepository.save(taskFromDB);
+    }
+
+    @DeleteMapping("/task/{id}")
+    public void delete(@PathVariable("id") Task task){
+
+        taskRepository.delete(task);
     }
 
 
--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -1,12 +1,19 @@
 $(function () {
 
+    const appendTask = function(data){
+        var taskCode = '<h4>' + data.name + '</h4>' +
+            'Описание:' + data.description + 'дата выполнения' + data.dateEnd;
+        $('#task-list')
+            .append('<div>' + taskCode + '</div>');
+    };
+
     $('.dateInput').datepicker({
         dateFormat:"dd.mm.yy",
     });
 
     $('#showAddTaskFormBtn').click(function() {
-        $("#todoForm form").trigger('reset');
-        $('#todoForm').css('display', 'flex');
+        $("#taskForm form").trigger('reset');
+        $('#taskForm').css('display', 'flex');
     });
 
     $('#taskForm').click(function(event){
@@ -15,21 +22,14 @@ $(function () {
         }
     });
 
-    $('#editForm').click(function(event){
-        if(event.target === this) {
-            $(this).css('display', 'none');
-        }
-    });
-
     var globTaskId;
 
-    //Open form with task information
-    $(document).on("click", ".taskLink", function() {
+    $(document).on("click", ".task-link", function() {
         var link = $(this);
         var taskId = $(this).data('id');
         $("#editForm").css("display", "flex");
         $.ajax({
-            url: "/task/",
+            url: "/task/" + taskId,
             method: "GET",
             success: function(response) {
                 globTaskId = taskId;
@@ -46,48 +46,8 @@ $(function () {
         });
     });
 
-    // //Saving task
-    // $("#saveTaskBtn").click(function() {
-    //     var formData = {
-    //         "name": $("#editTaskName").val(),
-    //         "dateStart": $("#editStartDate").val(),
-    //         "dateEnd": $("#editEndDate").val(),
-    //         "description": $("#editTaskDescription").val()
-    //     };
-    //     $.ajax({
-    //         url: "/task/",
-    //         method: 'PUT',
-    //         contentType: "application/json; charset=utf-8",
-    //         dataType: "json",
-    //         data: JSON.stringify(formData),
-    //         success: function(response) {
-    //             getAllToDo();
-    //             $('#editForm').css('display', 'none');
-    //         },
-    //         error: function(response) {
-    //             if (response.status === 404) {
-    //                 alert('Задание не найдено.');
-    //             }
-    //         }
-    //     });
-    // });
-
-    // //Deleting task
-    // $("#deleteTodoBtn").click(function() {
-    //     $.ajax({
-    //         url: baseUrl + globToDoId,
-    //         method: "DELETE",
-    //         success: function() {
-    //             getAllToDo();
-    //             $("#editForm").css("display", "none");
-    //         },
-    //         error: function (error) {
-    //             console.log(error);
-    //         }
-    //     });
-    // });
-
     //Adding task
+
     $("#addNewTaskBtn").click(function() {
         var formData = {
             "name" : $("#newTaskName").val(),
@@ -96,17 +56,60 @@ $(function () {
             "description" : $("#newTaskDescription").val()
         };
         $.ajax({
-            url: "/tasks",
+            url: "/task/",
             method: "POST",
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             data: JSON.stringify(formData, null, '\t'),
             success: function() {
-                getAllToDo();
+                appendTask();
             },
             error: function (error) {
                 console.log(error);
             }
         });
     });
+
+    // Edit task
+
+    $("#saveTaskBtn").click(function() {
+        var formData = {
+            "name": $("#editTaskName").val(),
+            "dateStart": $("#editStartDate").val(),
+            "dateEnd": $("#editEndDate").val(),
+            "description": $("#editTaskDescription").val()
+        };
+        $.ajax({
+            url: "/task/" + globTaskId,
+            method: 'PUT',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(formData),
+            success: function(response) {
+                getAllToDo();
+                $('#editForm').css('display', 'none');
+            },
+            error: function(response) {
+                if (response.status === 404) {
+                    alert('Задание не найдено.');
+                }
+            }
+        });
+    });
+
+    //Deleting task
+    $("#deleteTaskBtn").click(function() {
+        $.ajax({
+            url: "/task/" + globTaskId,
+            method: "DELETE",
+            success: function() {
+                getAllToDo();
+                $("#editForm").css("display", "none");
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
 });
